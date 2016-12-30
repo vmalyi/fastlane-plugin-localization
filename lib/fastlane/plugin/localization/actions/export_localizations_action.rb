b3bd281a9e6b67f9854caa83b6c9d0bfcf75be3c
Emil <emil@appunite.com>
2016-12-30 11:35:21 +0100

    improve exporting

--- lib/fastlane/plugin/localization/actions/export_localizations_action.rb
+++ lib/fastlane/plugin/localization/actions/export_localizations_action.rb
@@ -2,12 +2,24 @@ module Fastlane
   module Actions
     class ExportLocalizationsAction < Action
       def self.run(params)
+
+        #
+        xcodeproj_path = params[:xcodeproj]
         destination_path = params[:destination_path]
-        project = params[:project]
+        languages = params[:languages] ? params[:languages].split(",") : []
 
-        UI.message("Exporting localizations from #{project} to #{destination_path} folder")
+        #
+        UI.message("Exporting localizations from #{xcodeproj_path} to #{destination_path} folder")
 
-        sh "xcodebuild -exportLocalizations -localizationPath #{destination_path} -project #{project}"
+        #
+        command = "xcodebuild -exportLocalizations "
+        command << "-localizationPath #{destination_path} "
+        command << "-project #{xcodeproj_path} "
+        languages.each { |language| command << "-exportLanguage #{language} " }
+
+        #
+        Fastlane::Actions.sh command, log: true
+
       end
 
       def self.description
@@ -21,15 +33,29 @@ module Fastlane
       def self.available_options
         [
            FastlaneCore::ConfigItem.new(key: :destination_path,
-                                   env_name: "DESTINATION_PATH",
+                                   env_name: "FL_LOCALIZATIONS_DESTINATION_PATH",
                                 description: "Destination path where XLIFF will be exported to",
-                                   optional: false,
-                                       type: String),
-           FastlaneCore::ConfigItem.new(key: :project,
-                                   env_name: "PROJECT",
+                                   optional: true,
+                              default_value: "xliffs"),
+           FastlaneCore::ConfigItem.new(key: :xcodeproj,
+                                   env_name: "FL_LOCALIZATIONS_PROJECT",
                                 description: "Project to export localizations from",
-                                   optional: false,
+                              default_value: Dir["*.xcodeproj"].first,
+                               verify_block: proc do |value|
+                                         UI.user_error!("Please pass the path to the project, not the workspace") if value.end_with? ".xcworkspace"
+                                         UI.user_error!("Could not find Xcode project") unless File.exist?(value)
+                                       end),
+           FastlaneCore::ConfigItem.new(key: :languages,
+                                   env_name: "FL_LOCALIZATIONS_EXPORT_LANGUAGES",
+                                description: "List of languages to export",
+                                   optional: true,
                                        type: String),
+        ]
+      end
+
+      def self.example_code
+        [
+          'export_localizations(xcodeproj: "Project.xcodeproj", destination_path: "localizations", languages: "en,pl")'
         ]
       end
 
